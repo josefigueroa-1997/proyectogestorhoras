bfc6ecffaaaf164bec710e4efd794ad507bfe5ce
unknown <you@example.com>
2024-10-15 19:57:06 -0300

    mejorando gantt

--- Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a2ca49a7cd35cabc56dfcb1d64af3da2e615f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6259ee5b5e9b676a05be0828bb428b2f7c4414")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
