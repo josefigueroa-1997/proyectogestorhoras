64d19d6bce0fabcae5e554517faead0912baf87c
unknown <you@example.com>
2024-10-24 22:59:56 -0300

    trabajando en metas

--- Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecde7fb4c6a6a706f3151edb7bef95540a39f3bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435366237e71c99e341183e5e79bab526f4fd661")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
