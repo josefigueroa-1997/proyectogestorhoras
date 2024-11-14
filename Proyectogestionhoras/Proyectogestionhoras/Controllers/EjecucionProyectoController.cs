78f6cfa5ffa3091f27c094b418b799f31e7f9ca1
unknown <you@example.com>
2024-11-14 15:40:07 -0300

    trabajando en modulo presupuesto

---
+++ Proyectogestionhoras/Proyectogestionhoras/Controllers/EjecucionProyectoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyectogestionhoras.Models;
+using Proyectogestionhoras.Services;
+using static iText.StyledXmlParser.Jsoup.Select.Evaluator;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Proyectogestionhoras.Controllers
+{
+    public class EjecucionProyectoController : Controller
+    {
+        private readonly ProyectoService proyectoService;
+        private readonly FacturaService facturaService;
+        public EjecucionProyectoController(ProyectoService proyectoService, FacturaService facturaService)
+        {
+            this.proyectoService = proyectoService;
+            this.facturaService = facturaService;
+        }
+
+        public async Task<IActionResult> SeleccionarProyecto(int? id, int? idcliente, string? nombre, int? idtipoempresa, int? statusproyecto, string? numproyecto, int? idtipologia, int? unidadneg, int? idccosto, int? idusuario)
+        {
+            var proyectos = await proyectoService.ObtenerProyectos(id, idcliente, nombre, idtipoempresa, statusproyecto, numproyecto, idtipologia, unidadneg, idccosto, idusuario);
+            ViewBag.Proyectos = proyectos;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RedirigirActividad()
+        {
+            int idproyecto = int.Parse(Request.Form["idproyecto"].ToString());
+            HttpContext.Session.SetInt32("numproyecto", idproyecto);
+            return RedirectToAction("SeleccionarActividad", "EjecucionProyecto");
+        }
+        public IActionResult SeleccionarActividad() {
+
+            return View();
+        }
+        public async Task<IActionResult> ForecastIngreso(int? id, int? idcliente, string? nombre, int? idtipoempresa, int? statusproyecto, string? numproyecto, int? idtipologia, int? unidadneg, int? idccosto, int? idusuario)
+        {
+            var proyecto = await proyectoService.ObtenerProyectos(id, idcliente, nombre, idtipoempresa, statusproyecto, numproyecto, idtipologia, unidadneg, idccosto, idusuario);
+            var factura = await facturaService.RecuperarFacturas(id);
+            ViewBag.Proyecto = proyecto;
+            ViewBag.Factura = factura;
+            return View();
+        }
+
+        public async Task<IActionResult> ForecastCostos(int? id, int? idcliente, string? nombre, int? idtipoempresa, int? statusproyecto, string? numproyecto, int? idtipologia, int? unidadneg, int? idccosto, int? idusuario)
+        {
+            var proyecto = await proyectoService.ObtenerProyectos(id, idcliente, nombre, idtipoempresa, statusproyecto, numproyecto, idtipologia, unidadneg, idccosto, idusuario);
+            ViewBag.Proyecto = proyecto;
+            return View();
+        }
+    }
+}
--- Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d34193522e1d59391993ff448f110e825261e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6652d001a0ebf554b148a73ab4745889f438fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
