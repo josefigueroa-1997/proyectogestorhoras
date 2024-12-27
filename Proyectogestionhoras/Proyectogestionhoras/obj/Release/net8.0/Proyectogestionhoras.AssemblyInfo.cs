36bba14335ea8380cd73a3b5a0a0e682c1472435
unknown <you@example.com>
2024-12-27 14:33:50 -0300

    trabajando en flujo de caja de proyecto

--- Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e717c6d3918d6972638e64383591e4de58d7e55e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423445335b19ee9813e1cc506be88c1b56640d09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
