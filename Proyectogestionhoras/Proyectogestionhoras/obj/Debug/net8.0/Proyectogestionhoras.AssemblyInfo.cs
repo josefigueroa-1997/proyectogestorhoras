afb42b96e4d1461ce5f0b8878719eea9eef0fa4b
unknown <you@example.com>
2025-02-27 15:40:53 -0300

    modificando tabla sintesis

--- Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98c4a69232eef695dfb0ad863fc88a849a7c1cd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca9cd023cf28962b4020a4aa67b8c074d4ce44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
