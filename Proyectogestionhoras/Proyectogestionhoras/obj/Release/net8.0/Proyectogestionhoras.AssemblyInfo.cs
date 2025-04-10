d2b564788e5ebb7d852092db032b5753d612ff93
Pepe <you@example.com>
2025-04-09 20:33:18 -0400

    corrigiendo error forecast reporte consolidado

--- Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Release/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd9dc4c5035b23e0a625837846c8c288e1fbf76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b58c82bb9631bca6774e567624028e38ab0cf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
