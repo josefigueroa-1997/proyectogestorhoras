78e0ea613a7831576483ffe8c65eb110d3a490e1
Pepe <you@example.com>
2025-08-19 14:05:53 -0400

    terminado pago forecast automatico

--- Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
+++ Proyectogestionhoras/Proyectogestionhoras/obj/Debug/net8.0/Proyectogestionhoras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1c8908a367fa9c83aaaf7109f4c6ef08e42c46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac9f2f3bedd73dd4cb5db7f3277b8963deb612c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectogestionhoras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
